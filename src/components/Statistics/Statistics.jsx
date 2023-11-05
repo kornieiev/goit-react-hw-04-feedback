8d747304adb4c8d7989e17ce24df4ff9928d0ca1
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2023-11-05 19:44:45 +0100

    bug fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { List } from './Statistics.styled';
+import { List } from './Statistics.styled.js';
 
 export default function Statistics({
   good,
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const List = styled.list`
+export const List = styled.li`
   margin-bottom: 5px;
   font-size: 17px;
 `;
